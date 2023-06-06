8c47989837f2c91b809d15feadbee1c81a44c13a
databiryani23 <134163530+databiryani23@users.noreply.github.com>
2023-06-06 19:02:11 -0400

    Update streamlitapp.py
    Added breakfast and menu

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,2 +1,10 @@
 import streamlit
+
+#Add title to streamlit app
 streamlit.title('My first app')
+
+#Add breakfast and menu
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
