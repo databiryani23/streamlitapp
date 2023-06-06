dd8bc877f67ef7735da58f784237d72c0a93f3f5
databiryani23 <134163530+databiryani23@users.noreply.github.com>
2023-06-06 19:04:44 -0400

    Update streamlitapp.py
    changed header
    added emojis 
    added avocado and toast

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,8 +3,15 @@ import streamlit
 #Add title to streamlit app
 streamlit.title('My first app')
 
-#Add breakfast and menu
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+#Add breakfast and menu.    
+
+
+
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+
+
+streamlit.text('🥑🍞 Avocado and Toast')
