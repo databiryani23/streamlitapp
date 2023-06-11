66ce37e0117992ee1bbd6a0a4094265e8ee50450
databiryani23 <134163530+databiryani23@users.noreply.github.com>
2023-06-11 14:56:32 -0400

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-
+import requests
 #Add title to streamlit app
 streamlit.title('My first app')
 
@@ -21,6 +21,7 @@ streamlit.text('🥑🍞 Avocado and Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
+
 ##Read file from s3
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -28,3 +29,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list.loc[fruits_selected])
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
